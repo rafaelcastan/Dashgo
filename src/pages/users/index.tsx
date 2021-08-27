4774b29ac260a30e8d12c86795d8b1edd4cbcb6a
rafaelcastan <rc.castanheira190@gmail.com>
2021-08-27 15:27:20 -0300

    Add Page Navigation and More responsiveness

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,17 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import {Header} from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return(
         <Box>
             <Header />
@@ -15,32 +22,33 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        
-                        <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="pink"
-                          leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-                        >
-                            Criar novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                            >
+                                Criar novo
+                            </Button>
+                        </Link>
                     </Flex>
 
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                { isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]} >
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -49,7 +57,7 @@ export default function UserList() {
                                         <Text fontSize="small" color="gray.300">rc.castanheira190@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril, 2021</Td>
+                                { isWideVersion && <Td>04 de Abril, 2021</Td>}
                                 <Td>
                                     <Button
                                       as="a"
@@ -58,7 +66,7 @@ export default function UserList() {
                                       colorScheme="purple"
                                       leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
                                     >
-                                        Editar
+                                        {isWideVersion ? "Editar" : ""}
                                     </Button>
                                 </Td>
                             </Tr>
